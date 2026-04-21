c620c9ad296fc8781319c4af9912cb7a884332bf
Heisdawrld <Heisdawrld@users.noreply.github.com>
2026-04-21 19:52:41 +0000

    chore: remove all root-level debug and test scripts to clean up the repository

--- test.js
+++
@@ -1,6 +0,0 @@
-import db from './src/config/database.js';
-async function test() {
-  const res = await db.execute('SELECT * FROM historical_matches LIMIT 1');
-  console.log(res.rows[0]);
-}
-test();
